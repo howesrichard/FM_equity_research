b04a209936650dca99902594b5c20613b49bacb2
domkhoury <dkho0328@uni.sydney.edu.au>
2025-03-26 08:42:09 +0000

    FINAL

--- excel_reader.py
+++ excel_reader.py
@@ -1,11 +1,7 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import requests as rq
 import lxml
 import fpdf
-import yfinance as yf
-import ipywidgets as ipy
 
 # Read the excel file
 df = pd.read_excel('example_files/DCF_test.xlsx', sheet_name="Sheet1", usecols='A:L', skiprows=0, nrows=4)
