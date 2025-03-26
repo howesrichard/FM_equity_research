e396e37a969fc5e157db306ea2e895e054042cfc
bruceleearc <nathan.yang7153@gmail.com>
2025-03-26 08:33:55 +0000

    nathan

---
+++ equity research report generator <3/# Scraper.py
@@ -0,0 +1,12 @@
+# Scraper
+import requests as requests
+import pandas as pd
+import yfinance as yf
+import matplotlib.pyplot as plt
+
+def scraper():
+    print('Code: ALD, Code: Ampol, Market Cap = 6.9B, Chairperson: Steven Gregg, HQ:Sydbey')
+
+    return()
+
+scraper()
