43bb7d38750a438deb7840110170427fe1fa4801
domkhoury <dkho0328@uni.sydney.edu.au>
2025-03-26 08:06:50 +0000

    cleaning up excel

--- excel_reader.py
+++ excel_reader.py
@@ -10,11 +10,5 @@ import ipywidgets as ipy
 # Read the excel file
 df = pd.read_excel('example_files/DCF_test.xlsx', sheet_name="Sheet1", usecols='A:L', skiprows=0, nrows=4)
 
-# We can check if Python and Excel achieve the same NPV value
+excel_npv = df.iloc[-1,1] 
 
-excel_npv = df.iloc[-1,1] # get the NPV value from the last row and second column
-
-python_npv = sum(df.iloc[0, 1:] * df.iloc[1, 1:]) # calculate the NPV using Python
-
-print(f'Excel NPV: {excel_npv:,.2f}')
-print(f'Python NPV: {python_npv:,.2f}')
