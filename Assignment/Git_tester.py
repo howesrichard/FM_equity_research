7153c52d7bb3b320960b3bd63c8e90c89d2f9109
Hugh ODonnell <s4740073@uq.edu.au>
2025-04-18 22:46:59 +1000

    Committed with no changes

--- Assignment/Git_tester.py
+++ Assignment/Git_tester.py
@@ -1,1 +1,13 @@
-print("hello")
+import pandas as pd
+
+# URL of the webpage containing the dividend table
+url = "https://www.bendigoadelaide.com.au/investor-centre/dividends/fully-paid-ordinary-shares/"
+
+# Read all tables from the webpage
+tables = pd.read_html(url)
+
+# Assuming the first table is the dividend history table
+df = tables[0]
+
+# Display the DataFrame
+print(df.head())
