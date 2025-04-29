263093e3dee02ad9a6d1b9301c07887cb7dd34cc
Josh Stam <j.stamatellos@student.uq.edu.au>
2025-04-29 14:03:24 +1000

    stock price chart and headline puller

---
+++ Assignment/Stock_Price_Chart.py
@@ -0,0 +1,11 @@
+
+# Stock Price Chart for Bendigo Bank (BEN.AX) from 2020-04-27 to 2025-04-27
+
+import yfinance as yf
+Bendigo = yf.download(
+    tickers = 'BEN.AX', 
+    start='2020-04-27', 
+    end='2025-04-27'
+    )
+Bendigo
+Bendigo['Close'].plot()
